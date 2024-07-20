9cf98031f7927e720a9a9cc935ef04e041f1e77b
wildaaa12 <134575949+wildaaa12@users.noreply.github.com>
2024-07-20 07:35:27 +0700

    update halaman detail data

--- lib/detail_data.dart
+++ lib/detail_data.dart
@@ -54,7 +54,14 @@ class _UserDetailPageState extends State<UserDetailPage> {
             'address': _addressController.text,
           }, SetOptions(merge: true));
 
-          _showConfirmationDialog();
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data berhasil diperbarui')));
+
+          // Refresh user data after update
+          await _fetchUserData();
+
+          setState(() {
+            _isEditing = false;
+          });
         } catch (e) {
           ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Gagal memperbarui data: $e')));
         }
@@ -62,59 +69,34 @@ class _UserDetailPageState extends State<UserDetailPage> {
     }
   }
 
-  void _showConfirmationDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Konfirmasi Perubahan'),
-          content: Text('Apakah Anda ingin menyimpan perubahan?'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Tidak'),
-              onPressed: () {
-                Navigator.of(context).pop(); // Tutup dialog
-              },
-            ),
-            TextButton(
-              child: Text('Ya'),
-              onPressed: () {
-                Navigator.of(context).pop(); // Tutup dialog
-                setState(() {
-                  _isEditing = false;
-                });
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data berhasil diperbarui')));
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail Data Anda'),
+        title: Text('Detail Data Anda', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.teal,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: _isEditing ? _buildEditForm() : _buildUserInfo(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: _isEditing ? _buildEditForm() : _buildUserInfo(),
+        ),
       ),
-      floatingActionButton: !_isEditing
-          ? FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  _isEditing = true;
-                });
-              },
-              child: Icon(Icons.edit),
-            )
-          : FloatingActionButton(
-              onPressed: _updateUserDetails,
-              child: Icon(Icons.save),
-            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_isEditing) {
+            if (_formKey.currentState!.validate()) {
+              _updateUserDetails();
+            }
+          } else {
+            setState(() {
+              _isEditing = true;
+            });
+          }
+        },
+        child: Icon(_isEditing ? Icons.save : Icons.edit),
+        backgroundColor: Colors.teal,
+      ),
     );
   }
 
@@ -122,43 +104,58 @@ class _UserDetailPageState extends State<UserDetailPage> {
     return Container(
       padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300, width: 2.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
         borderRadius: BorderRadius.circular(12.0),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(
-              FirebaseAuth.instance.currentUser?.photoURL ?? '',
+          Center(
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                FirebaseAuth.instance.currentUser?.photoURL ?? '',
+              ),
+              backgroundColor: Colors.grey.shade200,
             ),
           ),
           SizedBox(height: 20),
-          _buildInfoField('Nama', _nameController.text),
-          _buildInfoField('Email', _emailController.text),
-          _buildInfoField('Telepon', _phoneController.text),
-          _buildInfoField('Alamat', _addressController.text),
+          _buildInfoField('Nama', _nameController),
+          _buildInfoField('Email', _emailController),
+          _buildInfoField('Telepon', _phoneController),
+          _buildInfoField('Alamat', _addressController),
         ],
       ),
     );
   }
 
-  Widget _buildInfoField(String label, String value) {
+  Widget _buildInfoField(String label, TextEditingController controller) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(label, style: TextStyle(fontSize: 18)),
+          Text(label, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           SizedBox(height: 5),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey.shade300, width: 1.0),
-              borderRadius: BorderRadius.circular(8.0),
+          TextFormField(
+            controller: controller,
+            readOnly: true,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.grey.shade200,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide.none,
+              ),
             ),
-            child: Text(value, style: TextStyle(fontSize: 16)),
           ),
         ],
       ),
@@ -168,58 +165,76 @@ class _UserDetailPageState extends State<UserDetailPage> {
   Widget _buildEditForm() {
     return Form(
       key: _formKey,
-      child: ListView(
-        children: [
-          TextFormField(
-            controller: _nameController,
-            decoration: InputDecoration(labelText: 'Nama'),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Mohon masukkan nama Anda';
-              }
-              return null;
-            },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Nama'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Mohon masukkan nama Anda';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Mohon masukkan email Anda';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: 'Telepon'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Mohon masukkan nomor telepon Anda';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _addressController,
+                decoration: InputDecoration(labelText: 'Alamat'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Mohon masukkan alamat Anda';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _updateUserDetails();
+                  }
+                },
+                child: Text('Simpan Perubahan'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                ),
+              ),
+            ],
           ),
-          TextFormField(
-            controller: _emailController,
-            decoration: InputDecoration(labelText: 'Email'),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Mohon masukkan email Anda';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _phoneController,
-            decoration: InputDecoration(labelText: 'Telepon'),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Mohon masukkan nomor telepon Anda';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _addressController,
-            decoration: InputDecoration(labelText: 'Alamat'),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Mohon masukkan alamat Anda';
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                _updateUserDetails();
-              }
-            },
-            child: Text('Simpan Perubahan'),
-          ),
-        ],
+        ),
       ),
     );
   }
